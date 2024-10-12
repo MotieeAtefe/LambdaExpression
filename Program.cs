9fbe161ed1943748cb71c094f414cade6efb1129
MotieeAtefe <motiee1377@gmaile.com>
2024-10-12 16:01:58 +0330

    list of employee sorted by name with ordrBy method

--- Program.cs
+++ Program.cs
@@ -1,10 +1,35 @@
-﻿namespace LambdaExpression
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace LambdaExpression
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<Employee> employees = new List<Employee>()
+          {
+              new Employee { Id=1, Name = "John", Salary = 3000 },
+              new Employee { Id=2, Name = "Alic", Salary = 500 },
+              new Employee { Id=3, Name = "Bob", Salary = 25000 },
+              new Employee { Id=4, Name = "Eve", Salary = 7000 },
+
+          };
+
+            Console.WriteLine("orginal list of employee");
+            DisplayEmployees(employees);
+            var sortedByName = employees.OrderBy(x => x.Name).ToList();
+
+
+
+        }
+        static void DisplayEmployees(List<Employee> employees)
+        {
+            foreach (var employee in employees) 
+            {
+                Console.WriteLine($"ID: {employee.Id}, Name: {employee.Name},"+$"Salary: {employee.Salary}$");
+            }
+            Console.WriteLine();
+
         }
     }
 }
